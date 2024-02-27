4358184c036728fdadb7a608789109bd4ae1c689
Abdullah Aziz <abdullahazees4@gmail.com>
2024-02-27 20:49:40 +0500

    goal model updated

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,26 +1,37 @@
 import mongoose from "mongoose";
 
-const goalSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User', // Assuming you have a User model
-    required: true
+const goalSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // Assuming you have a User model
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    goalValue: {
+      type: Number,
+      required: true,
+    },
+    goalDate: {
+      type: Date,
+      required: true,
+    },
+    goalStatus: {
+      type: String,
+      required: true,
+    },
+    dateWhenGoalCompleted: {
+      type: Date,
+      required: true,
+    },
   },
-  category: {
-    type: String,
-    required: true
-  },
-  goalValue: {
-    type: Number,
-    required: true
-  },
-  goalDate: {
-    type: Date,
-    required: true
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 // Create the Goal model
-const Goal = mongoose.model('Goal', goalSchema);
+const Goal = mongoose.model("Goal", goalSchema);
 
 export default Goal;
