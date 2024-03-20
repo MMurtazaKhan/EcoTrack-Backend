31d19ec9e9d6f0a8527a76c21e885c65aa8dc977
M-Abdullah-Bhatti <87266652+M-Abdullah-Bhatti@users.noreply.github.com>
2024-03-20 08:49:12 +0500

    food and mail api issue resolved

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,71 +1,76 @@
-
-
-
 // Create an object with corresponding carbon footprint values per kg
 const carbonFootprintData = {
-    wheat: 1.4,
-    maize: 1.1,
-    barley: 1.1,
-    oatmeal: 1.6,
-    rice: 4,
-    potatoes: 0.3,
-    cassava: 0.9,
-    caneSugar: 2.6,
-    beetSugar: 1.4,
-    otherPulses: 1.6,
-    peas: 0.8,
-    nuts: 0.2,
-    groundnuts: 2.4,
-    soymilk: 1,
-    tofu: 3,
-    soybeanOil: 6,
-    palmOil: 7.6,
-    sunflowerOil: 3.5,
-    rapeseedOil: 3.7,
-    oliveOil: 6,
-    tomatoes: 1.4,
-    onionsAndLeeks: 0.3,
-    rootVegetables: 0.3,
-    brassicas: 0.4,
-    otherVegetables: 0.5,
-    citrusFruit: 0.3,
-    bananas: 0.8,
-    apples: 0.3,
-    berriesAndGrapes: 1.1,
-    wine: 1.4,
-    otherFruit: 0.7,
-    coffee: 16.5,
-    darkChocolate: 18.7,
-    beefBeefHerd: 59.6,
-    beefDairyHerd: 21.1,
-    lambAndMutton: 24.5,
-    pigMeat: 7.2,
-    poultryMeat: 6.1,
-    milk: 2.8,
-    cheese: 21.2,
-    eggs: 4.5,
-    fishFarmed: 5.1,
-    shrimpsFarmed: 11.8
+  wheat: 1.4,
+  maize: 1.1,
+  barley: 1.1,
+  oatmeal: 1.6,
+  rice: 4,
+  potatoes: 0.3,
+  cassava: 0.9,
+  caneSugar: 2.6,
+  beetSugar: 1.4,
+  otherPulses: 1.6,
+  peas: 0.8,
+  nuts: 0.2,
+  groundnuts: 2.4,
+  soymilk: 1,
+  tofu: 3,
+  soybeanOil: 6,
+  palmOil: 7.6,
+  sunflowerOil: 3.5,
+  rapeseedOil: 3.7,
+  oliveOil: 6,
+  tomatoes: 1.4,
+  onionsAndLeeks: 0.3,
+  rootVegetables: 0.3,
+  brassicas: 0.4,
+  otherVegetables: 0.5,
+  citrusFruit: 0.3,
+  bananas: 0.8,
+  apples: 0.3,
+  berriesAndGrapes: 1.1,
+  wine: 1.4,
+  otherFruit: 0.7,
+  coffee: 16.5,
+  darkChocolate: 18.7,
+  beefBeefHerd: 59.6,
+  beefDairyHerd: 21.1,
+  lambAndMutton: 24.5,
+  pigMeat: 7.2,
+  poultryMeat: 6.1,
+  milk: 2.8,
+  cheese: 21.2,
+  eggs: 4.5,
+  fishFarmed: 5.1,
+  shrimpsFarmed: 11.8,
 };
 
+// Convert all object keys to lowercase
+const carbonFootprintDataLowercase = Object.keys(carbonFootprintData).reduce(
+  (acc, key) => {
+    acc[key.toLowerCase()] = carbonFootprintData[key];
+    return acc;
+  },
+  {}
+);
 
 const getCarbonFootprint = (req, res) => {
-    const { food, kg } = req.body;
+  const { food, kg } = req.body;
 
-    // Check if the food item exists in the carbonFootprintData
-    if (!(food.toLowerCase() in carbonFootprintData)) {
-        return res.status(404).json({ error: 'Food item not found' });
-    }
+  // Use the lowercase version of the food item to check in the modified carbonFootprintData
+  const foodLowercase = food.toLowerCase();
+  if (!(foodLowercase in carbonFootprintDataLowercase)) {
+    return res.status(404).json({ error: "Food item not found" });
+  }
 
-    // Calculate the carbon footprint based on the given kg
-    const carbonFootprintPerKg = carbonFootprintData[food.toLowerCase()];
-    const totalCarbonFootprint = carbonFootprintPerKg * parseFloat(kg);
+  // Calculate the carbon footprint based on the given kg
+  const carbonFootprintPerKg = carbonFootprintDataLowercase[foodLowercase];
+  const totalCarbonFootprint = carbonFootprintPerKg * parseFloat(kg);
 
-    const roundedCarbonFootprint = parseFloat(totalCarbonFootprint.toFixed(3));
+  const roundedCarbonFootprint = parseFloat(totalCarbonFootprint.toFixed(3));
 
-    // Send the total carbon footprint as the response
-    res.json({ carbonFootprint: roundedCarbonFootprint });
+  // Send the total carbon footprint as the response
+  res.json({ carbonFootprint: roundedCarbonFootprint });
 };
-
 // Export the API endpoint handler
-export {getCarbonFootprint}
+export { getCarbonFootprint };
