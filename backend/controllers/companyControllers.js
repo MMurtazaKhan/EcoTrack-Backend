76e3247dbc08302414edbbbb7d43197c8c4b126e
Murtaza Khan <murtaza10dec@gmail.com>
2024-07-11 14:32:46 +0500

    Company issue fixed

--- backend/controllers/companyControllers.js
+++ backend/controllers/companyControllers.js
@@ -9,6 +9,7 @@ const registerCompany = async (req, res) => {
     let { name, email, companyName, password } = req.body;
 
     let user = await User.findOne({ email });
+    // Company check added
     if (user && user.role === "company") {
       return res.status(400).json({ message: "Company already exists" });
     }
